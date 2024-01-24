4ed719fb300ffe1d1ec47fd4a41fd422411187f5
RomainMarques <77971683+RomainMarques@users.noreply.github.com>
2024-01-24 16:56:58 +0100

    create issue 6

--- tests/issue6.spec.js
+++ tests/issue6.spec.js
@@ -1,32 +1,12 @@
 // @ts-check
-// [Bug](https://github.com/RomainMarques/Software-Testing-K/issues/1) : When we want to edit the hiring date of an employee, the date is not editable.
+// [Bug](https://github.com/RomainMarques/Software-Testing-K/issues/6) : When we want to edit the hiring date of an employee, the date is not editable.
 const { test, expect } = require('@playwright/test');
 const { PlaywrightHRPage } = require('./Page/PlaywrightHRPage');
 
-test('has footer version 1.0.4', async ({ page }) => {
+test('create employee with numbers as name', async ({ page }) => {
   const playwrightHR = new PlaywrightHRPage(page);
 
-  await playwrightHR.verifyVersion('1.0.4');
-});
-
-test('employee is added', async ({ page }) => {
-  const playwrightHR = new PlaywrightHRPage(page);
-
-  await playwrightHR.addNewEmployee('John Doe', 'john.doe@email.fr', '32 avenue de la république', 'Adress Line 2', 'Paris', '75001', '2021-01-01', 'Software Engineer');
-
-  await playwrightHR.goToListEmployee();
-  await expect(page.getByRole('table')).toContainText('John Doe');
-  await expect(page.getByRole('table')).toContainText('john.doe@dmerej.info');
-});
-
-test("employee's hired date is edited", async ({ page }) => {
-  const playwrightHR = new PlaywrightHRPage(page);
-
-  await playwrightHR.goToListEmployee();
-
-  await page.click('text=Edit'); // there is no importance to choose the same employee because this bug is always present for all employees
-
-  await page.click('text=Update contract');
-
-  await expect(page.getByRole('insertion')).toBeEditable();
+  await playwrightHR.addNewEmployee('-10', 'john.doe@email.fr', '32 avenue de la république',
+      'Adress Line 2', 'Paris', '75001', '2021-01-01', 'Software Engineer');
+  await expect(page.locator('table')).not.toContainText('-10');
 });
