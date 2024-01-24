62dd100639b7bb2c5642983ce0fcde71688b567e
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2024-01-24 16:13:31 +0100

    Init Issue Playwright

---
+++ tests/Page/PlaywrightHRPage.js
@@ -0,0 +1,84 @@
+const { expect } = require('@playwright/test');
+
+exports.PlaywrightHRPage = class PlaywrightHRPage {
+
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
+  constructor(page) {
+    this.page = page;
+    this.getAddEmployeeLink = page.locator('a', { hasText: 'Add new employee' });
+    this.gettingAddEmployeeHeader = page.locator('h2', { hasText: 'Add new employee' });
+    this.getListEmployeeLink = page.locator('a', { hasText: 'List Employees' });
+    this.gettingListEmployeeHeader = page.locator('h2', { hasText: 'Employees' });
+    this.getListTeamsLink = page.locator('a', { hasText: 'List teams' });
+    this.gettingListTeamsHeader = page.locator('h2', { hasText: 'Teams' });
+    this.getAddNewTeamLink = page.locator('a', { hasText: 'Create new team' });
+    this.gettingAddNewTeamHeader = page.locator('h2', { hasText: 'Create new team' });
+    this.gettingResetDBLink = page.locator('a', { hasText: 'Reset database' });
+    this.gettingResetDBHeader = page.locator('h2', { hasText: 'Reset Database' });
+  }
+
+  async goto() {
+    await this.page.goto('https://k.hr.dmerej.info/');
+  }
+
+  async verifyVersion(version) {
+    this.goto();
+    await expect(this.page.getByRole('contentinfo')).toBeVisible();
+    const footerText = await this.page.getByRole('contentinfo').innerText();
+    await expect(footerText).toContain(version);
+  }
+
+  async goToAddEmployee() {
+    this.goto();
+    await this.getAddEmployeeLink.first().click();
+    await expect(this.gettingAddEmployeeHeader).toBeVisible();
+  }
+
+  async goToListEmployee() {
+    this.goto();
+    await this.getListEmployeeLink.first().click();
+    await expect(this.gettingListEmployeeHeader).toBeVisible();
+  }
+
+  async goToListTeams() {
+    this.goto();
+    await this.getListTeamsLink.first().click();
+    await expect(this.gettingListTeamsHeader).toBeVisible();
+  }
+
+  async goToAddNewTeam() {
+    this.goto();
+    await this.getAddNewTeamLink.first().click();
+    await expect(this.gettingAddNewTeamHeader).toBeVisible();
+  }
+
+  async addNewEmployee(name, email, address_line1, address_line2, city, zip_code, hiring_date, job_title) {
+    this.goToAddEmployee();
+    await this.page.fill('[name="name"]', name);
+    await this.page.fill('[name="email"]', email);
+    await this.page.fill('[name="address_line1"]', address_line1);
+    await this.page.fill('[name="address_line2"]', address_line2);
+    await this.page.fill('[name="city"]', city);
+    await this.page.fill('[name="zip_code"]', zip_code);
+    await this.page.fill('[name="hiring_date"]', hiring_date);
+    await this.page.fill('[name="job_title"]', job_title);
+
+    await this.page.click('button[type="submit"]');
+  }
+
+  async addTeam(name) {
+    this.goToAddNewTeam();
+    await this.page.fill('[name="name"]', name);
+    await this.page.click('button[type="submit"]');
+  }
+
+  async resetDB() {
+    this.goto();
+    await this.gettingResetDBLink.first().click();
+    await expect(this.gettingResetDBHeader).toBeVisible();
+    await this.page.click('button[type="submit"]');
+  }
+
+};
--- tests/issue7.spec.js
+++ tests/issue7.spec.js
@@ -1,5 +1,4 @@
 // @ts-check
-// [Bug](https://github.com/RomainMarques/Software-Testing-K/issues/1) : When we want to edit the hiring date of an employee, the date is not editable.
 const { test, expect } = require('@playwright/test');
 
 test('has footer version 1.0.4', async ({ page }) => {
@@ -22,7 +21,7 @@ test('has the "Add new employee" link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Add new employee' })).toBeVisible();
 });
 
-test('employee is added', async ({ page }) => {
+test('employee is added twice', async ({ page }) => {
   await page.goto('https://k.hr.dmerej.info/');
 
   // Click the get started link.
@@ -42,14 +41,33 @@ test('employee is added', async ({ page }) => {
   await expect(page).toHaveURL('https://k.hr.dmerej.info/employees');
   await expect(page.getByRole('table')).toContainText('John Doe');
   await expect(page.getByRole('table')).toContainText('john.doe@dmerej.info');
-});
 
-test("employee's hired date is edited", async ({ page }) => {
-  await page.goto('https://k.hr.dmerej.info/employees');
+  await page.goto('https://k.hr.dmerej.info/');
 
-  await page.click('text=Edit'); // there is no importance to choose the same employee because this bug is always present for all employees
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Add new employee' }).click();
 
-  await page.click('text=Update contract');
+  await page.fill('[name="name"]', 'John Doe');
+  await page.fill('[name="email"]', 'john.doe@dmerej.info');
+  await page.fill('[name="address_line1"]', '1 rue de la paix');
+  await page.fill('[name="address_line2"]', '75000 Paris');
+  await page.fill('[name="city"]', 'Paris');
+  await page.fill('[name="zip_code"]', '75000');
+  await page.fill('[name="hiring_date"]', '2012-12-12');
+  await page.fill('[name="job_title"]', 'Not an Engineer');
 
-  await expect(page.getByRole('insertion')).toBeEditable();
+  await page.click('button[type="submit"]');
+
+  await expect(page).toHaveURL('https://k.hr.dmerej.info/employees');
+
+  const tableRows = await page.$$('tbody tr');
+
+  const rowCount = await page.evaluate((rows) => {
+    return rows.filter(row => {
+      const textContent = row.textContent || '';
+      return textContent.includes('John Doe') && textContent.includes('john.doe@dmerej.info');
+    }).length;
+  }, tableRows);
+
+  expect(rowCount).toBe(1);
 });
